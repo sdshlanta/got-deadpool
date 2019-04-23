fddc40d7c8a1696dc884566f89ed3aecb2b96f4b
Mike Shlanta <sdshlanta@gmail.com>
2019-04-23 13:26:56 -0500

    UPDATES

--- config/environments.rb
+++ config/environments.rb
@@ -1,7 +1,7 @@
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
 configure :production, :development do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/got-deadpool')
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://fombgdhmgutkci:3d08f4af718d23ab717bdd6426eb6b44221a604133e6887ef62887a495405ad8@ec2-184-73-210-189.compute-1.amazonaws.com:5432/d87b23tpkt8s8s  ')
 
   ActiveRecord::Base.establish_connection(
       :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
